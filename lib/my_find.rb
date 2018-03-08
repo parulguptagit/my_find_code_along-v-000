19f9c8443c999737c442ebbd4d8a9e41b031cf31
Parul Gupta <parulguptasmail@gmail.com>
2018-03-08 22:37:19 +0000

    automatically backed up by learn

--- lib/my_find.rb
+++ lib/my_find.rb
@@ -1,5 +1,17 @@
 require 'pry'
 
-def my_find(collection)
-
+def my_find(collection) 
+   if block_given?
+      return_value 
+      i = 0 
+      while i < collection.length 
+        return_value =  yield(collection[i])
+        if return_value == TRUE
+          TRUE
+        end
+        i += 1 
+      end
+      FALSE
+   end
+  
 end
